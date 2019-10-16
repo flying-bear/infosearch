28576d8b2f4142e0b4f68f05322016399442e617
Andrew Lloyd <galka1999@gmail.com>
2019-10-16 05:24:52 +0300

    ELMo to class

--- proj/indexing_elmo.py
+++ proj/indexing_elmo.py
@@ -14,82 +14,119 @@ from constants import *
 
 tf.reset_default_graph()
 
-elmo_path = 'elmo'
 
-
-def crop_vec(vect, sent):
+class SearchELMo:
     """
-    deletes empty vectors at the end of a sentence
-
-    :param vect: np.array, vector from ELMo
-    :param sent: list of str, tokenized sentence
-    :return: np.array
+    indexes and searches a query by ELMo
     """
-    cropped_vector = vect[:len(sent), :]
-    cropped_vector = np.mean(cropped_vector, axis=0)
-    return cropped_vector
+    def __init__(self, path_elmo_matrix="", data=data_lemm, elmo_path="elmo"):
+        """
+        computes or loads indexed tf-idf matrix for search
+        :param path_elmo_matrix: string, path to a pickle file with elmo matrix (loaded from file if given)
+        :param data: lemmatized version of DataSet from constants, data_lemm by default
+        :param elmo_path: str, path to a folder with elmo model
+        """
+        self.data = data
+        self.batcher, self.sentence_character_ids, self.elmo_sentence_input = load_elmo_embeddings(elmo_path)
+        if path_elmo_matrix:
+            self.matrix = self.load(path_elmo_matrix)
+        else:
+            self.matrix = self.index()
+
+    @staticmethod
+    def load(path_elmo_matrix):
+        """
+        loads data form a given path
+
+        :param path_elmo_matrix: string, path to a pickle file with elmo matrix
+        :return: np.ndarray, elmo matrix from file
+        """
+        with open(path_elmo_matrix, 'rb') as f:
+            matrix = pickle.load(f)
+        return matrix
+
+    @staticmethod
+    def crop_vec(vect, sent):
+        """
+        deletes empty vectors at the end of a sentence
+
+        :param vect: np.array, vector from ELMo
+        :param sent: list of str, tokenized sentence
+        :return: np.array
+        """
+        cropped_vector = vect[:len(sent), :]
+        cropped_vector = np.mean(cropped_vector, axis=0)
+        return cropped_vector
+
+    def doc_to_vec(self, text):
+        """
+        creates a vector out of a document using fasttext model
+
+        :param text: string to be transformed into vector
+        :return: np.array, mean vector of word vectors in provided text
+          """
+        tokens = [tokenize(text)]
+        with tf.Session() as sess:
+            sess.run(tf.global_variables_initializer())
+            vector = self.crop_vec(get_elmo_vectors(sess, tokens, self.batcher, self.sentence_character_ids,
+                                                    self.elmo_sentence_input)[0], tokens[0])
+        return vector
+
+    def index(self, path="elmo_matrix.pickle"):
+        """
+        indexing texts using ELMo
+
+        :return: matrix of document vectors
+        """
+        tokenized = [tokenize(q) for q in self.data.texts]
+
+        with tf.Session() as sess:
+            # It is necessary to initialize variables once before running inference.
+            sess.run(tf.global_variables_initializer())
+            indexed = []
+            for i in range(200, len(tokenized)+1, 200):
+                sentences = tokenized[i-200: i]
+                elmo_vectors = get_elmo_vectors(sess, sentences,
+                                                self.batcher, self.sentence_character_ids, self.elmo_sentence_input)
+
+                for vect, sent in zip(elmo_vectors, sentences):
+                    cropped_vector = self.crop_vec(vect, sent)
+                    indexed.append(cropped_vector)
+
+        matrix = np.array(indexed)
+        with open(path, 'wb') as f:
+            pickle.dump(matrix, f)
+        return matrix
+
+    def search(self, text, n=10):
+        """
+        searches a given query in elmo model, returns top n results
+
+        :param text: str, text to search for
+        :param n: int, how many most relevant results to return, 10 by default
+        :return: list of tuples (float, str), cos_sim to the text, document text
+        """
+        query = " ".join(preprocess(text))
+        vector = self.doc_to_vec(query)
+        cos_sim_relevance = [cos_sim(vector, doc_vec) for doc_vec in np.array(self.matrix)]
+        relevance_sorted_document_ids_top_n = enum_sort_tuple(cos_sim_relevance)[:n]
+        return [(metric, self.data.texts[index]) for index, metric in relevance_sorted_document_ids_top_n]
 
 
-batcher, sentence_character_ids, elmo_sentence_input = load_elmo_embeddings(elmo_path)
+def main():
+    # start = time()
+    # SearchELMo()  # save the precomputed matrix
+    # print(f"indexing took {time()-start} sec")
+    start = time()
+    elmo = SearchELMo("elmo_matrix.pickle")
+    print(f"loading took {time()-start} sec")
+    start = time()
+    print("\n".join(map(str, elmo.search("я так хочу спать нет сил"))))
+    print(f"searching took {time()-start} sec")
+    start = time()
+    print(elmo.search("почему я расковыриваю лицо и губы"))
+    print(f"searching took {time() - start} sec")
 
 
-def doc_to_vec_elmo(text):
-    """
-    creates a vector out of a document using fasttext model
-
-    :param text: string to be transformed into vector
-    :return: np.array, mean vector of word vectors in provided text
-      """
-    tokens = [tokenize(text)]
-    with tf.Session() as sess:
-        sess.run(tf.global_variables_initializer())
-        vector = crop_vec(get_elmo_vectors(sess, tokens, batcher, sentence_character_ids,
-                                           elmo_sentence_input)[0], tokens[0])
-    return vector
-
-
-def elmo_indexing(tokenized):
-    """
-    indexing texts using ELMo
-    :param tokenized: list of lists of str, tokenized documents from the corpus
-
-    :return: matrix of document vectors
-    """
-    with tf.Session() as sess:
-        # It is necessary to initialize variables once before running inference.
-        sess.run(tf.global_variables_initializer())
-        indexed = []
-        for i in range(200, len(tokenized)+1, 200):
-            sentences = tokenized[i-200: i]
-            elmo_vectors = get_elmo_vectors(
-                sess, sentences, batcher, sentence_character_ids, elmo_sentence_input)
-
-            for vect, sent in zip(elmo_vectors, sentences):
-                cropped_vector = crop_vec(vect, sent)
-                indexed.append(cropped_vector)
-    return indexed
-
-
-# elmo_doc2vec_corpus = elmo_indexing([tokenize(q) for q in data_lemm.texts[:2000]])
-# with open("2000_raw_elmo_matrix.pickle", 'wb') as f:
-#     pickle.dump(elmo_doc2vec_corpus, f)
-
-with open("2000_raw_elmo_matrix.pickle", 'rb') as f:
-    elmo_doc2vec_corpus = pickle.load(f)
-
-def search_elmo(text, n=10):
-    """
-    searches a given query in elmo model, returns top n results
-
-    :param text: str, text to search for
-    :param n: int, how many most relevant results to return, 10 by default
-    :return: list of tuples (float, str), cos_sim to the text, document text
-    """
-    query = " ".join(lemmatize(preprocess(text)))
-    vector = doc_to_vec_elmo(query)
-    cos_sim_relevance = [cos_sim(vector, doc_vec) for doc_vec in elmo_doc2vec_corpus]
-    relevance_sorted_document_ids_top_n = enum_sort_tuple(cos_sim_relevance)[:n]
-    return [(metric, data_lemm.texts[:2000][index]) for index, metric in relevance_sorted_document_ids_top_n]
-
-
-print(search_elmo("нет меня печальнее на свете"))
+if __name__ == "__main__":
+    main()
